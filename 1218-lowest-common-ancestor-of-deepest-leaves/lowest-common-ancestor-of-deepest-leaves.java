6b4b5c12ad0448001aad294e9f19d2b279844d5c
Subhankar Banerjee <subhankarbanerjee180@gmail.com>
2026-01-09 11:55:24 +0530

    Time: 1 ms (45.12%) | Memory: 46.3 MB (57.01%) - LeetSync

--- 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.java
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.java
@@ -13,23 +13,24 @@
  *     }
  * }
  */
-class Solution {
-
-    private int height(TreeNode root) {
-        if(root == null) return 0;
-        int l = height(root.left);
-        int r = height(root.right);
-        return 1 + Math.max(l, r);
+class Pair {
+    TreeNode node;
+    int dis;
+    Pair(TreeNode node, int dis) {
+        this.node = node;
+        this.dis = dis;
     }
-
+}
+class Solution {
+    private Pair sol(TreeNode root, int d) {
+        if(root == null) return new Pair(null, d);
+        Pair l = sol(root.left, d+1);
+        Pair r = sol(root.right, d+1);
+        if(l.dis == r.dis) return new Pair(root, l.dis);
+        else return l.dis > r.dis ? l : r;
+    }
     public TreeNode lcaDeepestLeaves(TreeNode root) {
-        int l = height(root.left);
-        int r = height(root.right);
-        if(l == r) return root;
-        TreeNode node;
-        if(l > r) node = lcaDeepestLeaves(root.left); 
-        else node = lcaDeepestLeaves(root.right);
-
-        return node;
+        Pair p = sol(root, 0);
+        return p.node;
     }
 }
