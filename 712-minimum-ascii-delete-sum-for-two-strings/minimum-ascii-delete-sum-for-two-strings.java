79422345880554c214bef54ed216ecbef4539207
Subhankar Banerjee <subhankarbanerjee180@gmail.com>
2026-01-10 12:22:46 +0530

    Time: 19 ms (75.26%) | Memory: 46.8 MB (13.26%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -1,19 +1,16 @@
 class Solution {
-
-    private int sol(int i, int j, String s1, String s2, int[][] dp) {
-        if(i == 0 && j == 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(i == 0) return sol(i, j-1, s1,s2, dp) + s2.charAt(j-1); 
-        if(j == 0) return sol(i-1, j, s1,s2, dp) + s1.charAt(i-1); 
-
-        if(s1.charAt(i-1) == s2.charAt(j-1)) return dp[i][j] = sol(i-1, j-1, s1, s2, dp);
-        else return dp[i][j] = Math.min(sol(i-1, j, s1, s2, dp) + s1.charAt(i-1), sol(i, j-1, s1, s2, dp) + s2.charAt(j-1));
-    }
-
     public int minimumDeleteSum(String s1, String s2) {
         int n = s1.length(), m = s2.length();
         int[][] dp = new int[n+1][m+1];
-        for(int[] i : dp) Arrays.fill(i, -1);
-        return sol(n, m, s1, s2, dp);
+        for(int i = 1; i <= n; i++) dp[i][0] = dp[i-1][0] + s1.charAt(i-1);
+        for(int j = 1; j <= m; j++) dp[0][j] = dp[0][j-1] + s2.charAt(j-1);
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(s1.charAt(i-1) == s2.charAt(j-1)) dp[i][j] = dp[i-1][j-1];
+                else dp[i][j] = Math.min(dp[i-1][j] + s1.charAt(i-1), dp[i][j-1] + s2.charAt(j-1));
+            }
+        }
+        return dp[n][m];
     }
 }
