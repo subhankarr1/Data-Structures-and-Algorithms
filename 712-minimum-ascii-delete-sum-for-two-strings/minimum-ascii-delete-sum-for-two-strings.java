26bb3680dfc103cbdb7f6b6b2d8888dc7242cae6
Subhankar Banerjee <subhankarbanerjee180@gmail.com>
2026-01-10 12:30:39 +0530

    Time: 20 ms (66.62%) | Memory: 46.3 MB (81.07%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -1,16 +1,19 @@
 class Solution {
     public int minimumDeleteSum(String s1, String s2) {
         int n = s1.length(), m = s2.length();
-        int[][] dp = new int[n+1][m+1];
-        for(int i = 1; i <= n; i++) dp[i][0] = dp[i-1][0] + s1.charAt(i-1);
-        for(int j = 1; j <= m; j++) dp[0][j] = dp[0][j-1] + s2.charAt(j-1);
+        int[] prev = new int[m+1];
+        int[] curr = new int[m+1];
+        for(int j = 1; j <= m; j++) prev[j] =prev[j-1] + s2.charAt(j-1);
 
         for(int i = 1; i <= n; i++) {
+            curr[0] = prev[0] + s1.charAt(i-1);
             for(int j = 1; j <= m; j++) {
-                if(s1.charAt(i-1) == s2.charAt(j-1)) dp[i][j] = dp[i-1][j-1];
-                else dp[i][j] = Math.min(dp[i-1][j] + s1.charAt(i-1), dp[i][j-1] + s2.charAt(j-1));
+                if(s1.charAt(i-1) == s2.charAt(j-1)) curr[j] = prev[j-1];
+                else curr[j] = Math.min(prev[j] + s1.charAt(i-1), curr[j-1] + s2.charAt(j-1));
             }
+            prev = curr;
+            curr = new int[m+1];
         }
-        return dp[n][m];
+        return prev[m];
     }
 }
