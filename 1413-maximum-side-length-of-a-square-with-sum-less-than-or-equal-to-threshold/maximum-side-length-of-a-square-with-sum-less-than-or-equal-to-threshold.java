8a69974ea4ace62315e6a2b1f236d416eca6b00d
Subhankar Banerjee <subhankarbanerjee180@gmail.com>
2026-01-19 17:42:33 +0530

    Time: 5 ms (99.49%) | Memory: 58 MB (18.78%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
@@ -1,40 +1,18 @@
 class Solution {
-    private boolean helper(int[][] mat, int[][] rs, int size, int row, int col, int threshold) {
-        int sum = 0;
-        for (int i = row; i < row + size; i++) {
-            sum += rs[i][col + size] - rs[i][col];
-            if (sum > threshold)
-                return false;
-        }
-        return true;
-    }
-
     public int maxSideLength(int[][] mat, int threshold) {
         int n = mat.length, m = mat[0].length;
-        int[][] rs = new int[n][m+1];
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                rs[i][j+1] = rs[i][j] + mat[i][j];
+        int[][] prefix = new int[n+1][m+1];
+        int ans = 0, size = 0;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1] + mat[i-1][j-1];
             }
         }
-        int low = 1, high = Math.min(n,m), ans = 0;
-        while(low <= high) {
-            boolean f = false;
-            int mid = (low + high) / 2;
-            for(int i = 0; i + mid <= n; i++) {
-                for(int j = 0; j + mid <= m; j++) {
-                    if(helper(mat, rs, mid, i, j, threshold)) {
-                        f = true;
-                        break;
-                    }
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j <= m; j++) {
+                while(i + size <= n && j + size <= m && prefix[i+size][j+size] - prefix[i+size][j] - prefix[i][j+size] + prefix[i][j] <= threshold) {
+                    ans = size++;
                 }
-            }
-            if(f) {
-                ans = mid;
-                low = mid + 1;
-            }
-            else {
-                high = mid - 1;
             }
         }
         return ans;
