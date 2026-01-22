b36ca167b6833d414a3af701ca574ecd27b49084
Subhankar Banerjee <subhankarbanerjee180@gmail.com>
2026-01-22 14:22:42 +0530

    Time: 7 ms (10.44%) | Memory: 46.7 MB (5.22%) - LeetSync

--- 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.java
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.java
@@ -1,31 +1,67 @@
-class Solution {
-    private boolean check(List<Integer> lst) {
-        for(int i = 0; i < lst.size() - 1; i++) {
-            if(lst.get(i) > lst.get(i+1)) return false;
-        }
-        return true;
+class Pair implements Comparable<Pair> {
+    int ind;
+    long val, sum;
+    Pair left, right;
+
+    Pair(int ind, long val) {
+        this.ind = ind;
+        this.val = val;
     }
+    public int compareTo(Pair other) {
+        if(this.right == null || other.right == null) {
+            return this.right == null ? 1 : -1;
+        }
+        long diff = this.sum - other.sum;
+        return diff != 0 ? (diff < 0 ? -1 : 1) : (this.ind - other.ind);
+    }
+}
 
+class Solution {
     public int minimumPairRemoval(int[] nums) {
-        List<Integer> lst = new ArrayList<>();
-        for(int num : nums) lst.add(num);   
-
-        if(check(lst)) return 0;
-        int cnt = 0;
-        while(!check(lst)) {
-            int sum = Integer.MAX_VALUE;
-            int ind = -1;
-            for(int i = 0; i < lst.size() - 1; i++) {
-                int currSum = lst.get(i) + lst.get(i+1);
-                if(currSum < sum) {
-                    sum = currSum;
-                    ind = i;
-                }
+        TreeSet<Pair> set = new TreeSet<>();
+        int misMatch = 0;
+        Pair prev = null;
+        for(int i = 0; i < nums.length; i++) {
+            Pair curr = new Pair(i, nums[i]);
+            if(prev == null) {
+                prev = curr;
             }
-            int element = lst.get(ind) + lst.get(ind+1);
-            lst.remove(ind+1);
-            lst.set(ind, element);
-            cnt++; 
+            else {
+                if(prev.val > curr.val) misMatch++;
+                prev.right = curr;
+                curr.left = prev;
+                prev.sum = prev.val + curr.val;
+                set.add(prev);
+                prev = curr;
+            }
+        }
+        set.add(prev);
+        int cnt = 0;
+        while(misMatch > 0) {
+            Pair best = set.pollFirst();
+            if(best.val > best.right.val) misMatch--;
+            best.sum = best.val + best.right.sum;
+            best.val = best.val + best.right.val;
+            Pair del = best.right;
+            best.right = del.right;
+
+            if(del.right != null) {
+                if(del.val > del.right.val) misMatch--;
+                del.right.left = best;
+                if(best.val > best.right.val) misMatch++;
+            }
+            set.remove(del);
+            set.add(best);
+            Pair left = best.left;
+            if(left != null) {
+                set.remove(left);
+                if(left.val > left.sum - left.val) misMatch--;
+                if(left.val > best.val) misMatch++;
+                left.sum = left.val + best.val;
+                left.right = best;
+                set.add(left);
+            }
+            cnt++;
         }
         return cnt;
     }
