ca306ecca94f4cbee3aea29f8d4ac8c8670f635d
Subhankar Banerjee <subhankarbanerjee180@gmail.com>
2026-01-25 17:37:32 +0530

    Time: 2 ms (46.39%) | Memory: 48.9 MB (43.75%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,16 +1,16 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length, n2 = nums2.length, i = 0, j = 0;
-        ArrayList<Integer> merge = new ArrayList<>();
+        int n1 = nums1.length, n2 = nums2.length, i = 0, j = 0, k = 0;
+        int[] merge = new int[n1 + n2];
         while(i < n1 && j < n2) {
-            if(nums1[i] < nums2[j]) merge.add(nums1[i++]);
-            else merge.add(nums2[j++]);
+            if(nums1[i] < nums2[j]) merge[k++] = nums1[i++];
+            else merge[k++] = nums2[j++];
         }
-        while(i < n1) merge.add(nums1[i++]);
-        while(j < n2) merge.add(nums2[j++]);
-        int n = merge.size();
-        if(n % 2 == 1) return merge.get(n / 2);
+        while(i < n1) merge[k++] = nums1[i++];
+        while(j < n2) merge[k++] = nums2[j++];
+        int n = n1 + n2;
+        if(n % 2 == 1) return merge[n / 2];
 
-        return (merge.get(n / 2 - 1) + merge.get(n / 2)) / 2.0;
+        return (merge[n / 2 - 1] + merge[n / 2]) / 2.0;
     }
 }
