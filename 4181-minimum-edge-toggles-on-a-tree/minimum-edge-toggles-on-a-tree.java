9f919ba3b8a053c03df781375502a9cf07b47c21
Subhankar Banerjee <subhankarbanerjee180@gmail.com>
2026-01-28 17:54:16 +0530

    Time: 116 ms (26.97%) | Memory: 300.9 MB (27.83%) - LeetSync

---
+++ 4181-minimum-edge-toggles-on-a-tree/minimum-edge-toggles-on-a-tree.java
@@ -0,0 +1,39 @@
+class Edge {
+    int neighbourNode;
+    int ind;
+    Edge(int neighbourNode, int ind) {
+        this.neighbourNode = neighbourNode;
+        this.ind = ind;
+    }
+}
+class Solution {
+    private void DFS(int u, int parent, List<List<Edge>> adj, int[] need, List<Integer> ans) {
+        for(Edge e : adj.get(u)) {
+            int v = e.neighbourNode;
+            int originalInd = e.ind;
+            if(v == parent) continue;
+            DFS(v, u, adj, need, ans);
+            if(need[v] == 1) {
+                ans.add(originalInd);
+                need[v] ^= 1;
+                need[u] ^= 1;
+            }
+        }
+    }
+    public List<Integer> minimumFlips(int n, int[][] edges, String start, String target) {
+        List<List<Edge>> adj = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        for(int i = 0; i < n; i++) adj.add(new ArrayList<>());
+        for(int i = 0; i < n-1; i++) {
+            int u = edges[i][0], v = edges[i][1];
+            adj.get(u).add(new Edge(v, i));
+            adj.get(v).add(new Edge(u, i));
+        }
+        int[] need = new int[n];
+        for(int i = 0; i < n; i++) need[i] = (start.charAt(i) != target.charAt(i)) ? 1 : 0;
+        DFS(0, -1, adj, need, ans);
+        if(need[0] == 1) return List.of(-1);
+        Collections.sort(ans);
+        return ans;
+    }
+}
